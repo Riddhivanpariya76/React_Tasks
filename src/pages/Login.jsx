47f7ef706c970b8940e537541bebb317d32636be
Riddhivanpariya76 <riddhivanpariya76@gmail.com>
2026-01-31 13:01:52 +0530

    [RV][react files added]

---
+++ src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='form-container'>
+      {/* Page Title */}
+      <h1 className='form-title'>Welcome Back</h1>
+
+      {/* Login Form */}
+      <form>
+        {/* Email Field */}
+        <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input type="email" id='email' name='email' placeholder='Enter Your Email' />
+        </div>
+
+        {/* Password Field */}
+        <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' name='password' placeholder='Enter Your Password' />
+        </div>
+
+        {/* Submit Button */}
+        <button type='submit' className='btn-primary'>Login</button>
+
+      </form>
+
+      {/* Link to Register Page */}
+      {/* <p className="link-text">
+        Dont't have an account? 
+      </p> */}
+
+    </div>
+  )
+}
+
+export default Login
